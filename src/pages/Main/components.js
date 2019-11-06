70969d88c0404b0bbb427f73cff54cbaf620b252
Mark <marcos98cf@gmail.com>
2019-11-06 10:09:07 -0300

    Styling messages and made dinamically added

--- src/pages/Main/components.js
+++ src/pages/Main/components.js
@@ -1,4 +1,98 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	background: rgb(240, 240, 240);
+	height: 100vh;
+`
+
+export const NavBar = styled.nav`
+	position: fixed;
+	z-index: 102;
+	height: 10vh;
+	width: 100%;
+	display: flex;
+	background: white;
+	box-shadow: 0 2px 6px 4px rgba(0, 0, 0, 0.2);
+`
+
+export const Logo = styled.div`
+	display: flex;
+	justify-self: flex-end;
+	margin-left: 10px;
+	font-weight: bold;
+	align-items: center;
+	transition: all 0.7s;
+
+	&:hover {
+		transform: scale(2) translateX(27px);
+	}
+`
+
+export const ChatArea = styled.div`
+	width: 70%;
+	overflow: auto;
+
+	div {
+		display: flex;
+		background: rgba(0, 0, 0, 0.7);
+		height: 5vh;
+		border-bottom: 1px solid white;
+		
+		p {
+		margin: auto 30px;
+			margin-left: auto;
+			color: white;
+		}
+	}
+
+	div.minha {
+		background: grey;
+
+		p {
+			margin: auto 30px;
+		}
+	}
+`
+
+export const PeopleArea = styled.div`
+	width: 30%;
+	border-left: 1px solid rgba(0, 0, 0, 0.2);
+	background: white;
+`
+
+export const MainContainer = styled.div`
+	width: 100%;
+	height: 80vh;
+	margin-top: 10vh;
+	display: flex;
+`
+
+export const TextArea = styled.div`
+	display: flex;
+	background: rgba(50, 50, 50, 0.8);
+	width: 100%;
+	height: 10vh;
+	box-shadow: 0 2px 6px 4px rgba(0, 0, 0, 0.2);
+
+	textarea {
+		width: 75%;
+		height: 50%;
+		margin: auto 20px;
+		box-shadow: 0 2px 6px 4px rgba(0, 0, 0, 0.2);
+		border: 1px solid rgba(0, 0, 0, 0.2);
+		word-break: break-word;
+		resize: none;
+		overflow: hidden;
+	}
+
+	button {
+		height: 50%;
+		width: 15%;
+		margin: auto 40px;
+		box-shadow: 0 2px 6px 4px rgba(0, 0, 0, 0.2);
+		border: 1px solid rgba(0, 0, 0, 0.2);
+		font-size: 1em;
+	}
 `
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import { Container } from './components';
+import { Container, NavBar, Logo, MainContainer, PeopleArea, ChatArea, TextArea } from './components';
 
 
 export default function Main() {
+
+
+	const [messages, setMessages] = useState([ {message: "Oi, tudo bem?", minha: true}, 
+					   {message: "Tudo, e você?", minha: false},
+					   {message: "E aí galera", minha: false}])
+
+	const [message, setMessage] = useState("")
+
+	const addMessage = message => {
+		if (message.trim() !== '') setMessages([...messages, {message, minha: true}])
+	}
+
+	const onEnterPress = (e) => {
+		if(e.key === "Enter" && e.shiftKey === false) {
+		  addMessage(e.target.value)
+		  setMessage("")
+		}
+	  }
+
 	return (
-		<Container/>
+		<Container>
+			<NavBar>
+				<Logo>Bate papo OUL</Logo>
+			</NavBar>
+			<MainContainer>
+				<ChatArea>
+					{ messages.map(message => (
+					<div className={message.minha ? "minha" : ""}>
+						<p>
+							{message.message}
+						</p>
+					</div>
+					)) }
+				</ChatArea>
+				<PeopleArea></PeopleArea>
+			</MainContainer>
+			<TextArea>
+				<textarea value={message} onChange={value => setMessage(value.target.value)} onKeyDown={onEnterPress}/>
+				<button onClick={() => addMessage(message)}>Enviar</button>
+			</TextArea>
+		</Container>
 	);
 }
